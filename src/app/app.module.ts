cc81dab0aa5817dc308e3a75a7e5e38f515a2486
Martin <martingitehi@live.com>
2018-05-07 20:41:20 +0300

    removed postinstall script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
-import {Http, HttpModule, Response, RequestOptions } from '@angular/http';
+import { Http, HttpModule, Response, RequestOptions } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { API } from '../services/api-services';
 
+const appRoutes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+]
+
 @NgModule({
   declarations: [
     AppComponent
-   
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [API],
   bootstrap: [AppComponent]
