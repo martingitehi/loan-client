f3d597df54d9c21090185fc36b166f2d966eff32
Martin <martingitehi@live.com>
2018-05-09 18:53:32 +0300

    added SSL support for heroku

--- boot.js
+++ boot.js
@@ -18,8 +18,8 @@ const forceSSL = function () {
 //serve over https
 app.use(forceSSL())
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/loan-amortization-client'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/loan-amortization-client/index.html'));
 });
